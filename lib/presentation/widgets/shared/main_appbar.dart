14fa4c6c921ef8d1b9c35af28dfebe677a8a340f
arr199 <abieledelso@gmail.com>
2024-08-04 09:56:46 +0100

    create mainAppBar

---
+++ lib/presentation/widgets/shared/main_appbar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final AppBar appBar = AppBar();
+
+  MainAppBar({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  Size get preferredSize => Size.fromHeight(appBar.preferredSize.height);
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme.primary;
+    return AppBar(
+      title: Text(title),
+      titleTextStyle: TextStyle(color: color, fontSize: 20),
+    );
+  }
+}
